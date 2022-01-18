b8435784a26abb6c481c5cc7ed7213418cecd4ae
Momoko OSHIRO <e205430@ie.u-ryukyu.ac.jp>
2022-01-18 20:44:45 +0900

    2nd commit

---
+++ report6/app/src/main/java/jp/ac/uryukyu/ie/e205430/Hand.java
@@ -0,0 +1,10 @@
+package jp.ac.uryukyu.ie.e205430;
+
+public class Hand {
+    final int gu = 1;
+    final int choki = 2;
+    final int pa = 3;
+
+    public int getHand();
+    public String getName();
+}
